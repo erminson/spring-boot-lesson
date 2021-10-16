4ace9b3f07af15585286c0f8add39dbb8c380f28
Lev Erminson <erminson@gmail.com>
2021-10-16 20:54:45 +0300

    Add two entities - CompanyEntity and EmployeeEntity

---
+++ src/main/java/ru/erminson/springboot/lesson/entity/EmployeeEntity.java
@@ -0,0 +1,36 @@
+package ru.erminson.springboot.lesson.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "employee")
+public class EmployeeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "birth_day")
+    private LocalDate birthDay;
+
+    private Integer salary;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private CompanyEntity company;
+}
